8a823122c645041d44c73c9c5687fa2fb269e4c0
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2024-02-24 14:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+function swapTheme() {
+        var appDiv = document.getElementById('app');
+        var swapButton = document.getElementById('swap');
+
+        // Check the current theme class
+        if (appDiv.classList.contains('night')) {
+            // If the current theme is theme1, switch to theme2
+            appDiv.classList.remove('night');
+            appDiv.classList.add('button_night');
+        } else {
+            // If the current theme is theme2 or any other, switch to theme1
+            appDiv.classList.remove('button_night');
+            appDiv.classList.add('night');
+        }
+    }
 
